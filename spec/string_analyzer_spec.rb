00a61443689378ffbeba672fc8da5ea5c088b568
Virendra Jadhav <virendrajadhav.dev@gmai.com>
2025-12-25 09:36:49 +0530

    Initial commit - Rspec learning project

---
+++ spec/string_analyzer_spec.rb
@@ -0,0 +1,16 @@
+require "string_analyzer"
+
+describe StringAnalyzer do
+    context "with valid input" do
+        it "should detect when a string contains vowels" do 
+            sa = StringAnalyzer.new
+            test_string = "uuu"
+            expect(sa.has_vowels? test_string).to be true
+        end
+        it "should detect when a string doesn't contain vowels" do
+            sa = StringAnalyzer.new
+            test_string = "bcdfg"
+            expect(sa.has_vowels? test_string).to be false
+        end
+    end
+end
